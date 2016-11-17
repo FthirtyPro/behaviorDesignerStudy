4deab61bbee9ae8ec1ae876165a00c0c5c07b46a
FthirtyPro <fthirtypro@163.com>
2016-11-18 00:31:19 +0800

    defense 重写。还不能认出Tagert...

---
+++ Assets/defense.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using BehaviorDesigner.Runtime;
+using BehaviorDesigner.Runtime.Tasks;
+
+public class defense : Action {
+
+
+    public SharedFloat viewDistance;
+    public SharedFloat fieldOfViewAngle;
+    public SharedFloat speed;
+    public SharedFloat angularSpeed;
+    public SharedTransform target;
+
+    private NavMeshAgent navMeshAgent;
+    private float sqrViewDistance;
+
+    public override void OnAwake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>(); 
+
+    }
+    public override void OnStart()
+    {
+        sqrViewDistance = viewDistance.Value * viewDistance.Value;
+        navMeshAgent.enabled = true;
+        navMeshAgent.destination = target.Value.position;
+        navMeshAgent.speed = speed.Value;
+        navMeshAgent.angularSpeed = angularSpeed.Value;
+    }
+
+    public override void OnEnd()
+    {
+        navMeshAgent.enabled = false;
+    }
+
+    public override  TaskStatus OnUpdate()
+    {
+        if(target ==null&& target.Value ==null)
+        {
+            Debug.Log("llll");
+            return TaskStatus.Failure;
+        }
+        float sqrDistance =(target.Value.position - transform.position).sqrMagnitude;
+        float angle = Vector3.Angle(transform.forward, target.Value.position - transform.position);
+        if(sqrDistance< sqrViewDistance && angle< fieldOfViewAngle.Value*0.5)
+        {
+            if(navMeshAgent.destination != target.Value.position)
+                {
+                navMeshAgent.destination = target.Value.position; 
+            }
+
+            return TaskStatus.Running;
+        }
+        else
+        {
+
+            return TaskStatus.Success;
+
+        }
+
+
+
+    }
+
+
+}
